a217829d0c5a8c979fe7909a329d6ffc41ad6cf0
Ivan <voyten2003@bk.ru>
2024-09-20 11:52:27 +0300

    3

--- first_steps/rl_project1.py
+++ first_steps/rl_project1.py
@@ -1,4 +1,34 @@
 import random
 import gym
 
-env = gym.make("CartPole-v1")
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+env = gym.make("CartPole-v1", render_mode="human")
+
+states = env.observation_space.shape[0]
+actions = env.action_space.n
+
+print(states)
+print(actions)
+
+# ep = 10
+# for episode in range(1, ep+1):
+#     state = env.reset()
+#     done = False
+#     score = 0
+#
+#     while not done:
+#         action = random.choice([0, 1])
+#         _, reward, done, _ = env.step(action)
+#         score += reward
+#         env.render()
+#
+#     print(f"Episode -- {episode}, Score -- {score}")
+#
+# env.close()
